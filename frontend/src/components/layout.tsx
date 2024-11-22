41ea19f7ff26de07284255c4549fe44c942d9522
olesien <linus-lindberg@outlook.com>
2024-11-22 13:06:24 +0100

    cleanup

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, NavLink } from 'react-router-dom'
 
 const navigation = [
-    { name: 'Home', href: '/' }, { name: 'About', href: '/about' }, {name:"Style", href:'/style'}
+    { name: 'Home', href: '/' }, { name: 'About', href: '/about' }, { name: "Style", href: '/style' }
 ]
 
 function classNames(...classes: string[]) { return classes.filter(Boolean).join(' ') }
@@ -10,22 +10,27 @@ export default function Layout() {
     //const { isDark, setIsDark } = useAppContext()
     return (
         <>
-            {navigation.map((item) => (
-                <NavLink
-                    key={item.name}
-                    to={item.href}
-                    className={({ isActive }) =>
-                        classNames(
-                            isActive
-                                ? 'bg-gray-900 text-white'
-                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'rounded-md px-3 py-2 text-sm font-medium'
-                        )
-                    }
-                >
-                    {item.name}
-                </NavLink>
-            ))}
+            <header>
+                <div className="logo"></div>
+                <nav>
+                    {navigation.map((item) => (
+                        <NavLink
+                            key={item.name}
+                            to={item.href}
+                            className={({ isActive }) =>
+                                classNames(
+                                    isActive
+                                        ? 'bg-gray-900 text-white'
+                                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                )
+                            }
+                        >
+                            {item.name}
+                        </NavLink>
+                    ))}
+                </nav>
+            </header>
             <main className='mt-12'>
                 <Outlet />
             </main>
