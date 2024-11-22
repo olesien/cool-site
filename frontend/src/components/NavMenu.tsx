f779e33c71cc62d81f7d030f18f18d34b346b174
olesien <linus-lindberg@outlook.com>
2024-11-22 14:38:26 +0100

    Added basic mobile nav

--- frontend/src/components/NavMenu.tsx
+++ frontend/src/components/NavMenu.tsx
@@ -1,5 +1,8 @@
 import { NavLink } from "react-router-dom"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
+import { useState } from "react";
+import useIsMobile from "@/hooks/useIsMobile";
 const options = [
     {
         name: "Woman",
@@ -28,16 +31,37 @@ const options = [
 ]
 
 export function NavMenu() {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [showCats, setShowCats] = useState<{ [key: string]: boolean }>({});
+    const isMobile = useIsMobile();
     return (
         <div className="cat-container">
-            <div className="categories">{options.map(cat =>
+
+            {!isMobile && <div className="desktop-categories">{options.map(cat =>
                 <div className="dropdown">
                     <span>{cat.name.toUpperCase()}</span>
                     <ul className="dropdown-content">
                         {cat.subcategories.map(subcat => <li><NavLink key={subcat.link} to={subcat.link}>{subcat.name}</NavLink></li>)}
                     </ul>
                 </div>
-            )}</div>
+            )}</div>}
+            {isMobile && <div className="mobile-categories">
+                <FontAwesomeIcon className="icon" icon={faBars} onClick={() => setShowDropdown(old => !old)} />
+                {showDropdown && <ul className="dropdown">
+                    {options.map(cat => {
+
+                        return <>
+                            <li onClick={() => setShowCats(oldCats => ({ ...oldCats, [cat.name]: !oldCats[cat.name] }))}>
+                                {cat.name.toUpperCase()}
+                            </li>
+                            {showCats[cat.name] && <>
+                                {cat.subcategories.map(subcat => <li className="subcatlink"><NavLink key={subcat.link} to={subcat.link}>{subcat.name}</NavLink></li>)}
+                            </>}
+                        </>
+                    }
+                    )}
+                </ul>}
+            </div>}
         </div>
     )
 }
