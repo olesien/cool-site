5f84398abe8d23471d776205af1b6f1486873deb
Linus <olesien2@gmail.com>
2024-11-23 14:22:37 +0100

    small letter fix

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,16 +1,18 @@
-import { NavMenu } from "@/components/navMenu"
+import { NavMenu } from "@/components/NavMenu";
 
 export default function Home() {
-    return <div>
-        <NavMenu />
-        <p>Home Page</p>
+    return (
         <div>
-            <img
-                src="/images/02142133700-e1-removebg-preview.png"
-                alt="Sample"
-                loading="lazy"
-                className="image"
-            />
+            <NavMenu />
+            <p>Home Page</p>
+            <div>
+                <img
+                    src="/images/02142133700-e1-removebg-preview.png"
+                    alt="Sample"
+                    loading="lazy"
+                    className="image"
+                />
+            </div>
         </div>
-    </div>
+    );
 }
