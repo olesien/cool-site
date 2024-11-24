e46d1487c7514163d5e33dcb047c123d1db8578b
Rositsa Nikolova <rositsa.se@gmail.com>
2024-11-24 11:10:43 +0100

    ItemCard and a hardcoded Item List added
    Hardcoded a list with 4 items that represent one collection on focus on main page.
    It is just a demo and can be worked out later.
    Some basic css added in separate files in case we decide to row back or not include it, also to ensure easier teamwork with less conflicts.

---
+++ frontend/src/components/ItemCard.tsx
@@ -0,0 +1,21 @@
+import '../itemCard.css';
+interface ItemProps {
+  name: string;
+  price: number;
+  image: string;
+}
+
+
+export function Item({ name, price, image }: ItemProps) {
+  return (
+    <div
+      className="item-card"
+    >
+      <img src={image} alt={name} className="item-image" />
+      <div className="item-details">
+        <h3 className="item-name">{name}</h3>
+        <p className="item-price">SEK {price.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+}
