6664c63e07e0fce6573df03e6002f90758428588
Mashreka <mashreka.mannan@gmail.com>
2024-11-26 13:58:18 +0100

    commit
    we have created a Help form

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,41 +1,46 @@
-import { Outlet, NavLink } from 'react-router-dom'
+import { Outlet, NavLink } from "react-router-dom";
 
 const navigation = [
-    { name: 'Home', href: '/' }, { name: 'About', href: '/about' }, { name: "Style", href: '/style' }
-]
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Style", href: "/style" },
+  { name: "Help", href: "/help" },
+];
 
-function classNames(...classes: string[]) { return classes.filter(Boolean).join(' ') }
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Layout() {
-    //const { isDark, setIsDark } = useAppContext()
-    return (
-        <>
-            <header>
-                <div className="logo">Cool Site</div>
-                <nav>
-                    <div className="inner-nav">
-                        {navigation.map((item) => (
-                            <NavLink
-                                key={item.name}
-                                to={item.href}
-                                className={({ isActive }) =>
-                                    classNames(
-                                        isActive
-                                            ? 'bg-gray-900 text-white'
-                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'rounded-md px-3 py-2 text-sm font-medium'
-                                    )
-                                }
-                            >
-                                {item.name}
-                            </NavLink>
-                        ))}
-                    </div>
-                </nav>
-            </header>
-            <main className='mt-12'>
-                <Outlet />
-            </main>
-        </>
-    )
+  //const { isDark, setIsDark } = useAppContext()
+  return (
+    <>
+      <header>
+        <div className="logo">Cool Site</div>
+        <nav>
+          <div className="inner-nav">
+            {navigation.map((item) => (
+              <NavLink
+                key={item.name}
+                to={item.href}
+                className={({ isActive }) =>
+                  classNames(
+                    isActive
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "rounded-md px-3 py-2 text-sm font-medium"
+                  )
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
+        </nav>
+      </header>
+      <main className="mt-12">
+        <Outlet />
+      </main>
+    </>
+  );
 }
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,27 +1,37 @@
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense } from "react";
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-} from 'react-router-dom'
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
 // LAYOUT
-const Layout = lazy(async () => await import('@/components/layout'))
+const Layout = lazy(async () => await import("@/components/layout"));
 
 // PAGE
-const Home = lazy(async () => await import('@/pages/home'))
+const Home = lazy(async () => await import("@/pages/home"));
+// PAGE
+const Help = lazy(async () => await import("@/pages/help/help"));
 
 export const publicRoutes = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path='/' element={<Layout />}>
-            <Route
-                index
-                element={
-                    <Suspense fallback={<></>}>
-                        <Home />
-                    </Suspense>
-                }
-            />
-        </Route>
-    )
-)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route
+        index
+        element={
+          <Suspense fallback={<></>}>
+            <Home />
+          </Suspense>
+        }
+      />
+      <Route
+        path="help"
+        element={
+          <Suspense fallback={<></>}>
+            <Help />
+          </Suspense>
+        }
+      />
+    </Route>
+  )
+);
