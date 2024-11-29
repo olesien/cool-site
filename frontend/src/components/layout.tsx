c90e079c6d0812fa9f06e749c07f48f1b4fcdf62
sami-elt <139684759+sami-elt@users.noreply.github.com>
2024-11-29 13:57:38 +0100

    updated admin nav

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -47,6 +47,39 @@ export default function Layout() {
 
                         )}
 
+                        {isLoggedIn && <> 
+                            <NavLink
+                                    to={'/admin/categories'}
+                                    className={({ isActive }) =>
+                                        classNames(
+                                            isActive
+                                                ? 'bg-gray-900 text-white'
+                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                        )
+                                    }
+                                >
+                                    Categories
+                                </NavLink>
+
+                                <NavLink
+                    
+                                    to={'/admin/products'}
+                                    className={({ isActive }) =>
+                                        classNames(
+                                            isActive
+                                                ? 'bg-gray-900 text-white'
+                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                        )
+                                    }
+                                >
+                                    Products
+                                </NavLink>
+                            
+                            
+                            </>}
+
                     </div>
                 </nav>
             </header>
